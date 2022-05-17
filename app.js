65a7013d1d6fcf6f9540cac32bbcac1c706813ca
dhruv50ae <linkdp2012@gmail.com>
2022-05-17 22:00:48 +0100

    Created weather app

--- app.js
+++ app.js
@@ -12,9 +12,16 @@ app.get("/", (req, res) => {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
       const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write(
+        "<h1>The temperature in London is " + temp + " degrees celcius</h1>"
+      );
+      res.write("<p>The weather is currently " + weatherDescription + "</p>");
+      res.write("<img src = " + imageURL + " />");
+      res.send();
     });
   });
-  res.send("The server is up and running");
 });
 
 app.listen(3000, () => {
